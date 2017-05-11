a3806c1bef0430ce0cf16327196e3e333250399c
Sadhana Bhandari <bhandari.sadhana1@gmail.com>
2017-05-10 17:52:42 -0700

    new exercises added

--- learn_python_the_hard_way/ex1.py
+++ learn_python_the_hard_way/ex1.py
@@ -5,3 +5,8 @@ print "This is fun."
 print "Yay! Printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not thouch this.'
+
+print "I could have code like this." #and the comment after is ignored
+
+#print "The will not run."
+print "This will run."
