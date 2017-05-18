0325b71d3d5b5b88fd00f2643c25e8b5da78df5f
Sadhana Bhandari <bhandari.sadhana1@gmail.com>
2017-05-17 19:55:50 -0700

    exercises

---
+++ learn_python_the_hard_way/ex34.py
@@ -0,0 +1,7 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#animal at animals[0] is bear
+#animal at animals[1] is python
+#animal at animals[2] is peacock
+#animal at animals[3] is kangaroo
+# the sixth animal is platypus
